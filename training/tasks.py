ab58369be829e9d140725e997f6a4643afa48d45
benedikt <benedikt.schenkel@student.hpi.de>
2021-04-01 13:06:50 +0200

    add msgpack serialization

--- training/tasks.py
+++ training/tasks.py
@@ -1,6 +1,5 @@
 import argparse
-import base64
-import json
+import msgpack
 import os
 import sys
 from io import BytesIO
@@ -11,7 +10,8 @@ from PIL import Image
 from celery import Celery
 
 from analysis.analyzer import Analyzer
-
+import logging
+logger = logging.getLogger(__file__)
 
 class AnalysisTask(celery.Task):
 
@@ -33,17 +33,21 @@ class AnalysisTask(celery.Task):
 
 broker_address = os.environ.get('BROKER_ADDRESS', 'localhost')
 app = Celery('wpi_demo', backend='rpc://', broker=f"pyamqp://guest@{broker_address}//")
+app.conf.update(
+    accept_content  = ['msgpack'],
+    task_serializer = 'msgpack',
+    result_serializer = 'msgpack',
+)
 
-
-@app.task(name='handwriting_determination', base=AnalysisTask)
+@app.task(name='handwriting_determination', base=AnalysisTask, serializer="msgpack")
 def analyze(task_data):
     analyze.initialize()
-    image = base64.b85decode(task_data['image'])
-    io = BytesIO(image)
-    io.seek(0)
+    bytes = msgpack.unpackb(task_data)
 
-    with Image.open(io) as the_image:
+    image_data = BytesIO(bytes)
+    image_data.seek(0)
+
+    with Image.open(image_data) as the_image:
         the_image = the_image.convert('RGB')
         analyzed_boxes = analyze.analyzer.get_analysis_grid(the_image, normalize_boxes=True)
-
     return analyzed_boxes
